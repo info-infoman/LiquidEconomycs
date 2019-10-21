79cd0000359b1ed3d6849d2433bf654d66768d5d
infoman <info_infoman@bk.ru>
2019-10-22 01:04:12 +0400

    first load

---
+++ app/src/main/java/com/example/liquideconomycs/MainActivity.java
@@ -0,0 +1,254 @@
+package com.example.liquideconomycs;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView.OnQRCodeReadListener;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback, OnQRCodeReadListener {
+
+    private static final int MY_PERMISSION_REQUEST_CAMERA = 0;
+    private static final int MY_PERMISSION_REQUEST_INTERNET = 0;
+
+    private ViewGroup mainLayout;
+
+    private TextView            resultTextView;
+    private QRCodeReaderView    qrCodeReaderView;
+    private CheckBox            flashlightCheckBox;
+    private CheckBox            enableDecodingCheckBox;
+    private PointsOverlayView   pointsOverlayView;
+    private Core                        core = new Core(new Core.Out(){
+        private static final String TAG = "Core";
+
+        @Override
+        public void onStart() {
+            Log.d(TAG, "Core is started!");
+        }
+
+        @Override
+        public void onError(Exception error) {
+            Log.e(TAG, "Error!", error);
+        }
+    });
+
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        mainLayout = (ViewGroup) findViewById(R.id.main_layout);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            initQRCodeReaderView();
+        } else {
+            requestCameraPermission();
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {} else {
+            requestINTERNETPermission();
+        }
+
+        ///////////init trie db//////////////////////////////////////////////////////
+        String nodeDir=getApplicationContext().getFilesDir().getAbsolutePath()+"/db";
+        File nodeDirReference=new File(nodeDir);
+        while (!nodeDirReference.exists()) {
+            copyAssetFolder(getApplicationContext().getAssets(), "db", nodeDir);
+        }
+        /////////////////////////////////////////////////////////////////////////////
+        if (nodeDirReference.exists()) {
+            core.Start(nodeDir);
+        }
+
+
+    }
+
+    /*private void wsOnConnected(){
+        client.send(new byte[] {(byte) 0xDE, (byte) 0xAD, (byte) 0xBE, (byte) 0xEF });
+        //client.disconnect();
+    }*/
+
+    @Override protected void onResume() {
+        super.onResume();
+
+        if (qrCodeReaderView != null) {
+            qrCodeReaderView.startCamera();
+        }
+    }
+
+    @Override protected void onPause() {
+        super.onPause();
+
+        if (qrCodeReaderView != null) {
+            qrCodeReaderView.stopCamera();
+        }
+    }
+
+    @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                                     @NonNull int[] grantResults) {
+        AtomicBoolean ret= new AtomicBoolean(true);
+        if (requestCode == MY_PERMISSION_REQUEST_CAMERA) {
+            ret.set(false);
+        }
+        if (requestCode == MY_PERMISSION_REQUEST_INTERNET) {
+            ret.set(false);
+        }
+        if(ret.get()){
+            return;
+        }
+        if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Snackbar.make(mainLayout, "Permission was granted.", Snackbar.LENGTH_SHORT).show();
+            if (requestCode == MY_PERMISSION_REQUEST_CAMERA) {
+                initQRCodeReaderView();
+            }
+        } else {
+            Snackbar.make(mainLayout, "Permission request was denied.", Snackbar.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    // Called when a QR is decoded
+    // "text" : the text encoded in QR
+    // "points" : points where QR control points are placed
+    @Override public void onQRCodeRead(String text, PointF[] points) {
+        //text= (String) stringFromJNI(text);
+        resultTextView.setText(text);
+        pointsOverlayView.setPoints(points);
+    }
+
+    private void requestCameraPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            Snackbar.make(mainLayout, "Camera access is required to display the camera preview.",
+                    Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                @Override public void onClick(View view) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[] {
+                            Manifest.permission.CAMERA
+                    }, MY_PERMISSION_REQUEST_CAMERA);
+                }
+            }).show();
+        } else {
+            Snackbar.make(mainLayout, "Permission is not available. Requesting camera permission.",
+                    Snackbar.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this, new String[] {
+                    Manifest.permission.CAMERA
+            }, MY_PERMISSION_REQUEST_CAMERA);
+        }
+    }
+
+    private void requestINTERNETPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)) {
+            Snackbar.make(mainLayout, "INTERNET access is required to display the INTERNET preview.",
+                    Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                @Override public void onClick(View view) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[] {
+                            Manifest.permission.INTERNET
+                    }, MY_PERMISSION_REQUEST_INTERNET);
+                }
+            }).show();
+        } else {
+            Snackbar.make(mainLayout, "Permission is not available. Requesting INTERNET permission.",
+                    Snackbar.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this, new String[] {
+                    Manifest.permission.INTERNET
+            }, MY_PERMISSION_REQUEST_INTERNET);
+        }
+    }
+
+    private void initQRCodeReaderView() {
+        View content = getLayoutInflater().inflate(R.layout.content_decoder, mainLayout, true);
+
+        qrCodeReaderView = (QRCodeReaderView) content.findViewById(R.id.qrdecoderview);
+        resultTextView = (TextView) content.findViewById(R.id.result_text_view);
+        flashlightCheckBox = (CheckBox) content.findViewById(R.id.flashlight_checkbox);
+        enableDecodingCheckBox = (CheckBox) content.findViewById(R.id.enable_decoding_checkbox);
+        pointsOverlayView = (PointsOverlayView) content.findViewById(R.id.points_overlay_view);
+
+        qrCodeReaderView.setAutofocusInterval(2000L);
+        qrCodeReaderView.setOnQRCodeReadListener(this);
+        qrCodeReaderView.setBackCamera();
+        flashlightCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                qrCodeReaderView.setTorchEnabled(isChecked);
+            }
+        });
+        enableDecodingCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                qrCodeReaderView.setQRDecodingEnabled(isChecked);
+            }
+        });
+        qrCodeReaderView.startCamera();
+    }
+
+    private static boolean copyAssetFolder(AssetManager assetManager, String fromAssetPath, String toPath) {
+        try {
+            String[] files = assetManager.list(fromAssetPath);
+            boolean res = true;
+
+            if (files.length==0) {
+                //If it's a file, it won't have any assets "inside" it.
+                res &= copyAsset(assetManager,
+                        fromAssetPath,
+                        toPath);
+            } else {
+                new File(toPath).mkdirs();
+                for (String file : files)
+                    res &= copyAssetFolder(assetManager,
+                            fromAssetPath + "/" + file,
+                            toPath + "/" + file);
+            }
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean copyAsset(AssetManager assetManager, String fromAssetPath, String toPath) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = assetManager.open(fromAssetPath);
+            new File(toPath).createNewFile();
+            out = new FileOutputStream(toPath);
+            copyFile(in, out);
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+            return true;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/liquideconomycs/Trie.java
@@ -0,0 +1,203 @@
+package com.example.liquideconomycs;
+
+import com.google.common.primitives.Ints;
+import com.google.common.primitives.Longs;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Trie {
+    RandomAccessFile TrieRoot;
+    RandomAccessFile TrieNodes;
+    private byte[] hashSize = new byte[20];
+    private static byte NODE = (byte)0xFF;
+    private static byte ACCOUNT = (byte)0x01;
+    private static String BREAK = "TRIE_BREAK_REDUCE";
+    private static byte[] EMPTY_STRING = null;
+    Trie(String nodeDir) throws FileNotFoundException {
+        TrieNodes= new RandomAccessFile(nodeDir+"/trieNodes.dat", "rw");
+        TrieRoot= new RandomAccessFile(nodeDir+"/trieRoot.dat", "rw");
+    }
+
+    class node {
+        byte[]   bType;
+        int      KeySize;
+        byte[]   bKey;
+        byte[]   bHash;
+        long     childSize;
+        int      age;
+        long     step;
+
+        node(byte[] a, int b, byte[] c, byte[] d, long e, int f, long g)
+        {
+            bType = a;
+            KeySize = b;
+            bKey = c;
+            bHash = d;
+            childSize = e;
+            age = f;
+            step = g;
+        }
+    }
+
+    public byte[] getHash(RandomAccessFile TrieNodes) throws IOException {
+        ArrayList dHashes = new ArrayList();
+
+        Long step;
+        for (step = 0L; step < TrieNodes.length();) {
+            TrieNodes.seek(step);
+            node NodeData = getData(step, TrieNodes);
+            dHashes.add(NodeData.bHash);//old hash
+            step = NodeData.step;
+        }
+
+        Collections.sort(dHashes, new Comparator<byte[]>() {
+            @Override
+            public int compare(byte[] x, byte[] y) {
+                for (int i = x.length - 1; i >= 0; i--) {
+                    int r = x[i] - y[i];
+                    if (r != 0) {
+                        return r;
+                    }
+                }
+                return 0;
+            }
+
+        });
+
+        ByteBuffer buffer = ByteBuffer.allocate(dHashes.size());
+        for (int i = 0; i < dHashes.size(); i++) {
+            buffer.put((byte[]) dHashes.get(i));
+        }
+
+        return  CRC64.fromBytes(buffer.array()).getBytes();
+    }
+
+    private node getData(Long step, RandomAccessFile trieNodes) throws IOException {
+        //long newLong = Longs.fromByteArray(oldLongByteArray);
+        //int newInt = Ints.fromByteArray(oldIntByteArray);
+        //trieNodes.seek(step);
+        byte[] bType = new byte[0];
+        trieNodes.read(bType,0,1);
+        //TrieNodes.seek(1);
+
+        byte[] bKeySize = new byte[0];
+        trieNodes.read(bKeySize,0,1);
+        int KeySize = Ints.fromByteArray(bKeySize);
+
+        //TrieNodes.seek(1);
+        byte[] bKey = new byte[KeySize];
+        trieNodes.read(bKey,0, KeySize);
+
+        //TrieNodes.seek(KeySize);
+
+        //TrieNodes.seek(8);
+        byte[] bHash = new byte[8];
+        byte[] bChildSize = new byte[8];
+        long childSize = 0L;
+        byte[] bAge = new byte[2];
+        int age = 0;
+        if(bType.equals(NODE)){
+            trieNodes.read(bHash,0, 8);
+            trieNodes.read(bChildSize,0, 8);//node
+            childSize = Longs.fromByteArray(bChildSize);
+            step = step+KeySize+childSize+18;
+        }else{
+            trieNodes.read(bAge,0, 2);//node
+            age = Ints.fromByteArray(bAge);
+            step = step+KeySize+12;
+        }
+
+
+        return new node (bType, KeySize, bKey, bHash, childSize, age, step);
+    }
+
+    private long exist(byte[] key, RandomAccessFile trieNodes, Long start, Long end) throws IOException {
+        Long step=start;
+        for (; step < end;) {
+            trieNodes.seek(step);
+            node d = getData(step, trieNodes);
+            step=d.step;
+            if(d.bKey.equals(key)) return step;
+        }
+        return -1;
+    }
+
+    // a reduce implementation you can "break" out of
+    private reduseCallBack reduce(byte[] accumulator, RandomAccessFile trieNodes, Long start, Long end , byte[] result) throws IOException {
+        for (int i = 1; i < accumulator.length+1; i++) {
+            reduseCallBack R = new reduseCallBack(result, Arrays.copyOfRange(accumulator, i-1, i), i-1, trieNodes, start, end);
+            if (R.MSG == BREAK) break;
+            return R;
+        }
+        return new reduseCallBack(result, null, 0, trieNodes, start, end);
+    }
+
+    class reduseCallBack {
+        byte[] val;
+        String MSG;
+        int getIndex;
+        reduseCallBack(byte[] newKey, byte[] letter, int currentIndex, RandomAccessFile trieNodes, Long start, Long end) throws IOException {
+
+            if (newKey.length>0 && exist(newKey, trieNodes, start, end) !=-1) {
+                getIndex = currentIndex; // save the current index so we know where to split the key
+                MSG=BREAK;
+            }
+            if (newKey.length>0){
+                ByteBuffer buffer = ByteBuffer.allocate(newKey.length+letter.length);
+                val= buffer.put(newKey).put(letter).array();
+            }else{
+                val=letter;
+            }
+
+        }
+    }
+
+    private byte[] reduceReverse(byte[] end, byte[] callback){
+        byte[] current;
+        for (int i = end.length; i > 0; i--) {
+            current = Arrays.copyOfRange(end, 0, i);
+            String val = callback(current, end, i);
+            if (val === BREAK) break
+                    // if this is reached, it didn't break so return the original
+                    // if the loop ends here since no match was found
+                    current = end;
+
+        }
+        return current;
+    }
+
+    //return age
+    public int get(byte[] key, RandomAccessFile trieNodes, Long start, Long end) throws IOException {
+        // if the key exists already, return it
+        start = exist(key, trieNodes, start, end);
+        if(start!= -1){
+            node d = getData(start, trieNodes);
+            return d.age;
+        }else{
+            reduseCallBack getKey = reduce(key, trieNodes, start, end, EMPTY_STRING);
+            int getIndex = getKey.getIndex;
+            //console.log(data)
+            Long step = exist(getKey.val, trieNodes, start, end);
+            if ( step!=-1) {
+                node d = getData(step, trieNodes);
+                return gett(Arrays.copyOfRange(getKey.val, getIndex, getKey.val.length), trieNodes, d.step-d.childSize, d.step);
+            } else {
+                // no matches
+                return 0;
+            }
+        }
+    }
+
+    private int gett(byte[] key, RandomAccessFile trieNodes, Long start, Long end) throws IOException {
+        return get(key, trieNodes, start, end);
+    }
+
+
+}
